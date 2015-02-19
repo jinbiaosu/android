1a4973bd70b871ef58a7c25e5b352cd7df6cffee
jinbiaosu <736703702@qq.com>
2015-02-19 20:48:10 +0800

    add AccountMS project

---
+++ AccountMS/src/com/open/accountsoft/activity/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.open.accountsoft.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
+
+import com.vein.accountsoft.activity.R;
+
+public class SplashActivity extends Activity {
+	private Button splash_loginButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		splash_loginButton = (Button) findViewById(R.id.splash_btn);
+		
+		AnimationSet animationSet=new AnimationSet(true);
+		AlphaAnimation alphaAnimation=new AlphaAnimation(0, 1);
+		alphaAnimation.setDuration(2000);
+		animationSet.addAnimation(alphaAnimation);
+		splash_loginButton.startAnimation(animationSet);
+
+
+		splash_loginButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(SplashActivity.this, Login.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+}
---
+++ AccountMS/src/com/open/accountsoft/model/Tb_pwd.java
@@ -0,0 +1,22 @@
+package com.open.accountsoft.model;
+
+public class Tb_pwd {
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Tb_pwd() {
+		super();
+	}
+
+	public Tb_pwd(String password) {
+		super();
+		this.password = password;
+	}
+}
